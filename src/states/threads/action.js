696a402e47a488480cd06ac82fe1775cdc16a732
rizalsuryana <rizal.suryn@gmail.com>
2025-01-19 19:31:35 +0700

    acton & reducer for Threads

--- src/states/threads/action.js
+++ src/states/threads/action.js
@@ -1,1 +1,157 @@
-// Action file for threads
+import { showLoading, hideLoading } from 'react-redux-loading-bar';
+import { toast } from 'react-toastify';
+import api from '../../utils/api';
+
+const ActionType ={
+  RECEIVE_THREADS: 'RECEIVE_THREADS',
+  ADD_THREAD: 'ADD_THREAD',
+  CLEAR_THREADS: 'CLEAR_THREADS',
+  UP_VOTE_THREAD: 'UP_VOTE_THREAD',
+  DOWN_VOTE_THREAD: 'DOWN_VOTE_THREAD',
+  NEUTRALIZE_THREAD: 'NEUTRALIZE_THREAD',
+  TOGGLE_CATEGORY_THREAD: 'TOGGLE_CATEGORY_THREAD',
+
+};
+
+
+const receiveThreadsActionCreator = (threads) => {
+  return {
+    type: ActionType.RECEIVE_THREADS,
+    payload: {
+      threads,
+    }
+  };
+};
+
+const addThreadActionCreator = (thread) => {
+  return {
+    type: ActionType.ADD_THREAD,
+    payload: {
+      thread
+    }
+  };
+};
+
+const clearThreadActionCreator = () => {
+  return {
+    type: ActionType.CLEAR_THREADS,
+  };
+};
+
+const toggleCategoryThreadActionCreator = (category) => {
+  return {
+    type: ActionType.TOGGLE_CATEGORY_THREAD,
+    payload: {
+      category,
+    }
+  };
+};
+
+const upVoteThreadActionCreator = ({ threadId, userId }) => {
+  return {
+    type: ActionType.UP_VOTE_THREAD,
+    payload: {
+      threadId,
+      userId,
+    }
+  };
+};
+
+const downVoteThreadActionCreator = ({ threadId, userId }) => {
+  return {
+    type: ActionType.DOWN_VOTE_THREAD,
+    payload:{
+      threadId,
+      userId
+    }
+  };
+};
+
+const neutralizeThreadActionCreator = ({ threadId, userId }) => {
+  return {
+    type: ActionType.NEUTRALIZE_THREAD,
+    payload: {
+      threadId,
+      userId,
+    }
+  };
+};
+
+const asyncAddThread = ({ title, body, category }) => {
+  return async (dispatch) => {
+    try {
+      dispatch(showLoading());
+      const responseThread = await api.createThread({ title, body, category });
+      dispatch(addThreadActionCreator(responseThread));
+      toast('Creating yor thread...');
+    } catch (error) {
+      toast.error(error?.message);
+    } finally {
+      dispatch(hideLoading());
+    }
+  };
+};
+
+const asyncUpVoteThread = (threadId) => {
+  return async (dispatch, getState) => {
+    const { authUser, threads } = getState();
+    try {
+      dispatch(showLoading());
+      dispatch(upVoteThreadActionCreator({ threadId, userId: authUser.id }));
+      await api.upVoteThread(threadId);
+    } catch (error) {
+      toast.error(error?.message);
+      dispatch(receiveThreadsActionCreator(threads));
+    } finally {
+      dispatch(hideLoading());
+    }
+  };
+};
+
+const asyncDownVoteThread = (threadId) => {
+  return async (dispatch, getState) => {
+    const { authUser, threads } = getState();
+    try {
+      dispatch(showLoading());
+      dispatch(downVoteThreadActionCreator({ threadId, userId: authUser.id }));
+      await api.downVoteThread(threadId);
+    } catch (error) {
+      toast.error(error?.message);
+      dispatch(receiveThreadsActionCreator(threads));
+    } finally {
+      dispatch(hideLoading());
+    }
+  };
+};
+
+const asyncNeutralizeVoteThread = (threadId) => {
+  return async (dispatch, getState) => {
+    const { authUser, threads } = getState();
+    try {
+      dispatch(showLoading());
+      dispatch(neutralizeThreadActionCreator({ threadId, userId: authUser.id }));
+      await api.neutralizeThreadVote(threadId);
+    } catch (error) {
+      toast.error(error?.message);
+      dispatch(receiveThreadsActionCreator(threads));
+    } finally {
+      dispatch(hideLoading());
+    }
+  };
+};
+
+
+export {
+  ActionType,
+  receiveThreadsActionCreator,
+  addThreadActionCreator,
+  clearThreadActionCreator,
+  toggleCategoryThreadActionCreator,
+  upVoteThreadActionCreator,
+  downVoteThreadActionCreator,
+  neutralizeThreadActionCreator,
+  asyncAddThread,
+  asyncDownVoteThread,
+  asyncNeutralizeVoteThread,
+  asyncUpVoteThread,
+};
--- src/states/threads/reducer.js
+++ src/states/threads/reducer.js
@@ -1,1 +1,56 @@
-// Reducer file for threads
+import { act } from 'react';
+import { ActionType } from './action';
+
+const threadsReducer = (threads = [], action = {}) => {
+  switch (action.type){
+  case ActionType.RECEIVE_THREADS:
+    return action.payload.threads;
+  case ActionType.ADD_THREAD:
+    return [action.payload.thread, ...threads];
+  case ActionType.CLEAR_THREADS:
+    return [];
+  case ActionType.UP_VOTE_THREAD:
+    return threads.map((thread)=> {
+      if (thread.id === action.payload.threadId){
+        return {
+          ...thread,
+          upVoteBy: thread.upVoteBy.includes(action.payload.userId)
+            ? thread.upVoteBy.filter((id)=> id !== action.payload.userId)
+            : thread.upVoteBy.concat([action.payload.userId]),
+          downVoteBy: thread.downVoteBy.filter((id)=> id !== action.payload.userId),
+        };
+      }
+      return thread;
+    });
+  case ActionType.DOWN_VOTE_THREAD:
+    return threads.map((thread)=> {
+      if (thread.id === action.payload.threadId){
+        return {
+          ...thread,
+          downVoteBy: thread.downVoteBy.includes(action.payload.userId)
+            ? thread.downVoteBy.filter((id)=> id !== action.payload.userId)
+            : thread.downVoteBy.concat([action.payload.userId]),
+          upVoteBy: thread.upVoteBy.filter((id)=> id !== action.payload.userId)
+        };
+      }
+      return thread;
+    });
+  case ActionType.NEUTRALIZE_THREAD:
+    return threads.map((thread)=> {
+      if (thread.id === action.payload.threadId){
+        return {
+          ...thread,
+          upVoteBy: thread.upVoteBy.filter((id)=> id !== action.payload.userId),
+          downVoteBy: thread.downVoteBy.filter((id)=> id !== action.payload.userId),
+        };
+      }
+      return thread;
+    });
+  default:
+    return threads;
+  }
+};
+
+
+
+export default threadsReducer;
